3bb59354394db94db2cde6f94aa819b009f0fd92
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2022-12-29 20:06:55 -0300

    Queen

--- src/main/UI.java
+++ src/main/UI.java
@@ -39,7 +39,7 @@ public class UI {
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
-            char column = s.charAt(0);
+            char column = s.toLowerCase().charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
         } catch(RuntimeException e) {
