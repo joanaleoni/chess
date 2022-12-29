ae00a7a64eaa84c22cd3e9dca49c478bd4e62dc5
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2022-12-29 05:28:26 -0300

    Project created

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author joana
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
